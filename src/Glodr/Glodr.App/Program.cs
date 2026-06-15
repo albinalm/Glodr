eea69ac4f8c07410f2c1634916ac169053014053
Albin Alm <albinalm@proton.me>
2026-06-15 09:49:53 +0200

    Add source files

---
+++ src/Glodr/Glodr.App/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Glodr.App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ src/Glodr/Glodr.Cli/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Glodr.Cli;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
